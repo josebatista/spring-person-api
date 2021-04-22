3765b7bbacc77e5858b6d532aaf1b0d0fe34f6fe
Jose Pereira <josebatistapereira@gmail.com>
2021-04-22 16:29:04 -0300

    developed find by id

--- src/main/java/com/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/service/PersonService.java
@@ -4,11 +4,13 @@ import com.digitalinnovation.personapi.dto.mapper.PersonMapper;
 import com.digitalinnovation.personapi.dto.request.PersonDTO;
 import com.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import com.digitalinnovation.personapi.entity.Person;
+import com.digitalinnovation.personapi.exception.PersonNotFoundException;
 import com.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -39,5 +41,16 @@ public class PersonService {
                 .stream()
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+//        Optional<Person> optionalPerson = personRepository.findById(id);
+//        if(optionalPerson.isEmpty()) {
+//            throw new PersonNotFoundException(id);
+//        }
+//        return personMapper.toDTO(optionalPerson.get());
+
+        Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+        return personMapper.toDTO(person);
     }
 }
